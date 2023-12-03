d575c6c653454b4303db1ba397826a54ec01d470
OlaffoJaszczur <olafsenisson@gmail.com>
2023-12-03 03:23:08 +0100

    changed a lot

--- emg.ino/emg.ino.ino
+++ emg.ino/emg.ino.ino
@@ -1,60 +1,22 @@
-#include <Servo.h>
+/*
+  ReadAnalogVoltage
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
+  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
 
-Servo myservo;  // create servo object to control a servo
+  This example code is in the public domain.
+*/
 
-int emgPin = A0;  // analog input pin for the EMG sensor
-int emgRawPin = A1;
-int emgRawValue = 0;
-int emgValue = 0; // variable to store the value read from the sensor
-int threshold = 600; // threshold value to detect muscle activity
-bool muscleConstricted = false; // flag to track muscle activity
-
-unsigned long lastConstrictionTime = 0; // time when the last muscle constriction was detected
-const unsigned long latency = 100; // latency in milliseconds (2000ms = 2 seconds)
-
-int currentPosition = 0; // current position of the servo
-int targetPosition = 0; // target position of the servo
-const int maxPosition = 300; // maximum position of the servo
-const int stepSize = 1; // step size for smooth movement
-
+// the setup routine runs once when you press reset:
 void setup() {
-    myservo.attach(3); // attaches the servo on pin 9 to the servo object
-    pinMode(emgPin, INPUT);
-    Serial.begin(9600);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-    emgValue = analogRead(emgPin); // read the value from the sensor
-    Serial.println(emgValue);
-
-    if (emgValue > threshold && !muscleConstricted) {
-        lastConstrictionTime = millis(); // store the time of muscle constriction
-        muscleConstricted = true;
-        targetPosition = maxPosition; // set target position
-    }
-
-    if (millis() - lastConstrictionTime > latency && muscleConstricted) {
-        // Gradually move to the target position
-        if (currentPosition < targetPosition) {
-            currentPosition += stepSize;
-            myservo.write(currentPosition);
-            delay(5); // delay for smoother transition
-        } else {
-            muscleConstricted = false; // reset the muscle activity flag
-            targetPosition = 0; // reset target position
-        }
-    }
-
-    // Gradually move back to the initial position
-    if (!muscleConstricted && currentPosition > 0) {
-        currentPosition -= stepSize;
-        myservo.write(currentPosition);
-        delay(5); // delay for smoother transition
-    }
-
-    myservo.write(map(analogRead(A0),0,1023,0,180));
-    delay(50);
-
-    emgRawValue = analogRead(emgRawPin); // read the value from the sensor
-    Serial.println(emgRawValue);
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  Serial.println(sensorValue);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,25 @@ int emgPin = A0;  // analog input pin for the EMG sensor
 int empRawPin = A1; // analog input pin for the EMG raw signal sensor
 int emgValue = 0; // variable to store the value read from the sensor
 int emgRawValue = 0; // variable to store the value read from the raw signal sensor
-int threshold = 600; // threshold value to detect muscle activity
-bool muscleConstricted = false; // flag to track muscle activity
+int threshold = 200; // threshold value to detect muscle activity
 
-unsigned long lastConstrictionTime = 0; // time when the last muscle constriction was detected
-const unsigned long latency = 100; // latency in milliseconds (2000ms = 2 seconds)
+const int numReadings = 10; // number of readings to smooth
 
-int currentPosition = 0; // current position of the servo
-int targetPosition = 0; // target position of the servo
-const int maxPosition = 300; // maximum position of the servo
-const int stepSize = 1; // step size for smooth movement
+int readings[numReadings]; // the readings from the analog input
+int readIndex = 0; // the index of the current reading
+int total = 0; // the running total
+int average = 0; // the average
 
 void setup() {
     myservo.attach(3); // attaches the servo on pin 9 to the servo object
     pinMode(emgPin, INPUT);
     pinMode(empRawPin, INPUT);
     Serial.begin(9600);
+
+    // initialize all the readings to 0:
+    for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+        readings[thisReading] = 0;
+    }
 }
 
 void loop() {
@@ -30,33 +33,37 @@ void loop() {
     emgRawValue = analogRead(empRawPin); //read the value from the raw signal sensor
     Serial.print(emgRawValue);
     Serial.print(" ");
-    Serial.println(emgValue);
+//    Serial.println(emgValue);
 
-    if (emgValue > threshold && !muscleConstricted) {
-        lastConstrictionTime = millis(); // store the time of muscle constriction
-        muscleConstricted = true;
-        targetPosition = maxPosition; // set target position
+
+//    myservo.write(map(analogRead(emgPin),0,1023,0,180));
+//    delay(100);
+
+    // subtract the last reading:
+    total = total - readings[readIndex];
+    // read from the sensor:
+    readings[readIndex] = analogRead(emgValue);
+    // add the reading to the total:
+    total = total + readings[readIndex];
+    // advance to the next position in the array:
+    readIndex = readIndex + 1;
+
+    // if we're at the end of the array...
+    if (readIndex >= numReadings) {
+        // ...wrap around to the beginning:
+        readIndex = 0;
     }
 
-    if (millis() - lastConstrictionTime > latency && muscleConstricted) {
-        // Gradually move to the target position
-        if (currentPosition < targetPosition) {
-            currentPosition += stepSize;
-            myservo.write(currentPosition);
-            delay(5); // delay for smoother transition
-        } else {
-            muscleConstricted = false; // reset the muscle activity flag
-            targetPosition = 0; // reset target position
-        }
-    }
+    // calculate the average:
+    average = total / numReadings;
+    // send it to the servo
+    myservo.write(map(average, 0, 600, 0, 180));
 
-    // Gradually move back to the initial position
-    if (!muscleConstricted && currentPosition > 0) {
-        currentPosition -= stepSize;
-        myservo.write(currentPosition);
-        delay(5); // delay for smoother transition
-    }
+    // print the average to the Serial Monitor:
+//    Serial.print("Average analog reading: ");
+//    Serial.println(average);
 
-    myservo.write(map(analogRead(emgPin),0,1023,0,180));
-    delay(50);
+
+
+    delay(1); // delay in between reads for stability
 }
