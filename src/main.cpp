45a8c4445e12da8d6bb090398d07cccb67966812
OlaffoJaszczur <olafsenisson@gmail.com>
2023-12-05 01:03:34 +0100

    data maching python plots added low pass filtering

--- src/main.cpp
+++ src/main.cpp
@@ -82,6 +82,90 @@ public:
     }
 };
 
+template <int order> // order is 1 or 2
+class LowPass
+{
+private:
+    float a[order];
+    float b[order+1];
+    float omega0;
+    float dt;
+    bool adapt;
+    float tn1 = 0;
+    float x[order+1]; // Raw values
+    float y[order+1]; // Filtered values
+
+public:
+    LowPass(float f0, float fs, bool adaptive){
+        // f0: cutoff frequency (Hz)
+        // fs: sample frequency (Hz)
+        // adaptive: boolean flag, if set to 1, the code will automatically set
+        // the sample frequency based on the time history.
+
+        omega0 = 6.28318530718*f0;
+        dt = 1.0/fs;
+        adapt = adaptive;
+        tn1 = -dt;
+        for(int k = 0; k < order+1; k++){
+            x[k] = 0;
+            y[k] = 0;
+        }
+        setCoef();
+    }
+
+    void setCoef(){
+        if(adapt){
+            float t = micros()/1.0e6;
+            dt = t - tn1;
+            tn1 = t;
+        }
+
+        float alpha = omega0*dt;
+        if(order==1){
+            a[0] = -(alpha - 2.0)/(alpha+2.0);
+            b[0] = alpha/(alpha+2.0);
+            b[1] = alpha/(alpha+2.0);
+        }
+        if(order==2){
+            float alphaSq = alpha*alpha;
+            double beta[] = {1, sqrt(2), 1};
+            float D = alphaSq*beta[0] + 2*alpha*beta[1] + 4*beta[2];
+            b[0] = alphaSq/D;
+            b[1] = 2*b[0];
+            b[2] = b[0];
+            a[0] = -(2*alphaSq*beta[0] - 8*beta[2])/D;
+            a[1] = -(beta[0]*alphaSq - 2*beta[1]*alpha + 4*beta[2])/D;
+        }
+    }
+
+    float filt(float xn){
+        // Provide me with the current raw value: x
+        // I will give you the current filtered value: y
+        if(adapt){
+            setCoef(); // Update coefficients if necessary
+        }
+        y[0] = 0;
+        x[0] = xn;
+        // Compute the filtered values
+        for(int k = 0; k < order; k++){
+            y[0] += a[k]*y[k+1] + b[k]*x[k];
+        }
+        y[0] += b[order]*x[order];
+
+        // Save the historical values
+        for(int k = order; k > 0; k--){
+            y[k] = y[k-1];
+            x[k] = x[k-1];
+        }
+
+        // Return the filtered value
+        return y[0];
+    }
+};
+
+// Filter instance
+LowPass<2> lp(20,1e3,true); // cut of frequency 20H
+
 Servo myservo;  // create servo object to control a servo
 
 int emgPin = A0;  // analog input pin for the EMG sensor
@@ -89,8 +173,7 @@ int empRawPin = A1; // analog input pin for the EMG raw signal sensor
 int emgValue = 0; // variable to store the value read from the sensor
 int emgRawValue = 0; // variable to store the value read from the raw signal sensor
 int threshold = 200; // threshold value to detect muscle activity
-int fs = 16000; // sampling frequeancy Hz
-float pi = 3.14; // rounded PI
+int fs = 1600; // sampling frequeancy Hz
 
 const int numReadings = 10; // number of readings to smooth
 
@@ -104,9 +187,9 @@ const float HighPassTarget = 20; //Target 20hz
 const float CentralFrequency = sqrt(LowPassTarget*HighPassTarget);
 const float Bandwidth = LowPassTarget-HighPassTarget;
 // Filter instance
-BandPass<2> lp(CentralFrequency,Bandwidth, fs,true);
+BandPass<2> bp(CentralFrequency,Bandwidth, fs,true);
 
-const int rmsWindowSize = 50; // Size of the RMS window
+const int rmsWindowSize = 150; // Size of the RMS window
 float rmsValues[rmsWindowSize]; // Array to hold the last 'rmsWindowSize' rectified values
 int rmsIndex = 0; // Index for the next RMS value
 float sumOfSquares = 0; // Sum of the squares of the last 'rmsWindowSize' values
@@ -143,7 +226,7 @@ void loop() {
     Serial.print(" ");
 
     // Apply the band-pass filter
-    float bandPassValue = lp.filt(rawValue);  // Variable to store band-pass filter output
+    float bandPassValue = bp.filt(rawValue);  // Variable to store band-pass filter output
 
     // Send the filtered value to the serial monitor
     Serial.print(bandPassValue);
@@ -155,6 +238,16 @@ void loop() {
     // Send the rectified value to the serial monitor
     Serial.print(rectifiedValue);
     Serial.print(" ");
+
+    // Apply the low-pass filter
+    float lowPassValue = lp.filt(rectifiedValue);  // Variable to store low-pass filter output
+
+    // Send the filtered value to the serial monitor
+    Serial.print(lowPassValue);
+    Serial.print(" ");
+
+    // Full-Wave Rectification
+    rectifiedValue = abs(lowPassValue);
 
     // Add the square of the rectified value to the sum
     sumOfSquares -= rmsValues[rmsIndex] * rmsValues[rmsIndex];
@@ -171,6 +264,7 @@ void loop() {
     Serial.print(rmsValue);
     Serial.print(" ");
 
+    // RMS of data from sig port
     // Update the RMS window and sum of squares
     emgSumOfSquares -= emgRmsWindow[emgRmsIndex] * emgRmsWindow[emgRmsIndex]; // Remove the oldest squared value
     emgRmsWindow[emgRmsIndex] = emgValue; // Insert the new value
